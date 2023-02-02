d1678b88c07a8dd08dcbafcad81e9e3e54fa9866
grace-Adiele8 <adielegrace8@gmail.com>
2023-02-02 12:52:41 +0100

    changes

--- src/components/Videodetail/VideoDetail.jsx
+++ src/components/Videodetail/VideoDetail.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { link, useParams} from 'react-router-dom';
+import { Link, useParams} from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
@@ -18,7 +18,7 @@ import { fetchFromAPI } from '../../utilits/fetchFromAPI';
 
 function VideoDetail() {
   const [videoDetail, setVideoDetail] = useState(null);
-  
+  const [videos, setVideos] = useState(null);
   const { id } = useParams();
   
   
@@ -28,13 +28,16 @@ function VideoDetail() {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
      .then((data) => setVideoDetail(data.items[0]));
   
-  }, [id]);
+     
+     fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+      .then((data) => setVideos(data.items[0]));   
+ 
+    }, [id]);
   
   
-  const { snippet: {title, channelId, channelTitle}, statistcs: {viewCount, likeCount}} = videoDetail;
+  // const { statistcs: {viewCount, likeCount}} = videoDetail;
   
-  
-  
+   
   return (
     <Box minHeight='95vh' >
       <Stack direction={{xs: 'column', md: 'row'}}>
@@ -42,10 +45,21 @@ function VideoDetail() {
         <Box sx={{width: '100%', position:'sticky', top:'86px'}}>
           <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls />
          <Typography color='#fff' variant='h5' fontWeight='bold' p={2}>
-          {title}
+          {videoDetail?.snippet?.title}
          </Typography>
         <Stack direction='row' justifyContent='space-between' sx={{color: '#fff'}} py={1} px={2} >
-
+         <Link to={`/channel/${videoDetail?.snippet?.channelId}`}>
+          <Typography variant={{ sm: 'subtitle1' , md: 'h6'}} color='#fff'>
+           {videoDetail?.snippet?.channelTitle}
+           <CheckCircle sx={{ fontSize: '12px' , color: 'gray' , ml: '5px'}} />
+          </Typography>
+         
+         </Link>
+         <Stack direction='row' gap='20px' alignItems='center'>
+          <Typography variant='body1' sx={{ opacity: 0.7}}>
+           {parseInt(videoDetail?.statistics?.viewCount).toLocaleString()} views
+          </Typography>
+         </Stack>
         </Stack>
         </Box>
       </Box>
